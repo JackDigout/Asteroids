8dee3b322919155396ec345b4a1ac072a094434c
jackdigout <jackdigout@gmail.com>
2025-01-29 15:51:04 -0500

    Added player icon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
@@ -10,14 +12,16 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(5) / 1000
-        print(f"FPS: {1/dt:.0f}")
 
 if __name__ == "__main__":
     main()
