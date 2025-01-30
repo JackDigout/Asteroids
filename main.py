c7aa44c6f27404b311cd315882568049cc0b74aa
jackdigout <jackdigout@gmail.com>
2025-01-29 21:27:53 -0500

    added shooting and collisions

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
+import sys
 
 def main():
     pygame.init()
@@ -14,9 +18,16 @@ def main():
 
     updateable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
     Player.containers = (updateable, drawable)
+    Shot.containers = (shots, updateable, drawable)
+    Asteroid.containers = (asteroids, updateable, drawable)
+    AsteroidField.containers = (updateable)
 
     player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    afield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -24,16 +35,30 @@ def main():
                 return
             
         updateable.update(dt)
+        
 
+        for asteroid in asteroids:
+            if player1.collision(asteroid):
+                print("Game Over!")
+                sys.exit()
+        
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collision(asteroid):
+                    asteroid.kill()
+            
         # Clear the screen by filling it with black color
         screen.fill("black")
         
         # Draw all objects in the `drawable` group
         for drawable_object in drawable:
             drawable_object.draw(screen)
-            
+        
+        
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
